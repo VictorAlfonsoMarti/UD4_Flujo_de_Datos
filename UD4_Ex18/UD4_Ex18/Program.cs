36af366a94729f33785adb96e52415cab697ef72
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:38:20 +0100

    Add files via upload
    24/12/2020 UD4_Ex18

---
+++ UD4_Ex18/UD4_Ex18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UD4_Ex18
+{
+    // 18) Haz el mismo ejercicio anterior con un bucle for.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x=0; x<=100; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
