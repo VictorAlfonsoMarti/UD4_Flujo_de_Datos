77ccb2e125a00f1b9cef48b4ef34d211d953fb5d
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:30:29 +0100

    Add files via upload
    24/12/2020 UD4_Ex11

---
+++ UD4_Ex11/UD4_Ex11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UD4_Ex11
+{
+    class Program
+    {
+        /* 11) Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+        dos.Si son iguales indicarlo también.Ves cambiando los valores para comprobar que
+        funciona.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número 1: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Número 2: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("{0} es más grande", x);
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("{0} es más grande", y);
+            }
+            else
+            {
+                Console.WriteLine("Los dos números son iguales");
+            }
+
+        }
+    }
+}
