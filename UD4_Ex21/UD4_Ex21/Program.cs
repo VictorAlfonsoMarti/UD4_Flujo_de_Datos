e813d963eb95d75374e99bfcee3558060d41366f
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:41:26 +0100

    Add files via upload
    24/12/2020 UD4_Ex21

---
+++ UD4_Ex21/UD4_Ex21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UD4_Ex21
+{
+    // 21) Crea una aplicación que nos pida un día de la semana y que nos diga si es un día laboral o no. Usa un switch para ello
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un día de la semana.");
+            string dia = Console.ReadLine();
+
+            switch (dia)
+            {
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana.");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es fin se semana.");
+                    break;
+                default:
+                    Console.WriteLine("El dia introducido es incorrecto.");
+                    break;
+            }
+        }
+    }
+}
