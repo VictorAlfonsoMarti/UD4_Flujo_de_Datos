b30268546827d422647f929ced1dbd285d99b2ca
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:37:32 +0100

    Add files via upload
    24/12/2020 UD4_Ex17

---
+++ UD4_Ex17/UD4_Ex17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UD4_Ex17
+{
+    //17) Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0; 
+
+            while (x <= 100)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
