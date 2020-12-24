9ec5a2cbbbf1aeb6e73cfba51fff7fd9564e17cb
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:35:03 +0100

    Add files via upload
    24/12/2020 UD4_Ex14

---
+++ UD4_Ex14/UD4_Ex14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UD4_Ex14
+{
+    /* 14) Haz una aplicación que calcule el área de un circulo (pi*R2). El radio se pedirá por
+    teclado (recuerda pasar de String a double). Usa la constante PI y el método pow de Math.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcualr el área de un circulo. Introduce el radio: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(radio, 2); //Llamamos al número pi y lo multiplicamos por el radio al cuadrado.
+
+            Console.WriteLine("El área es: {0}", area);
+
+        }
+    }
+}
