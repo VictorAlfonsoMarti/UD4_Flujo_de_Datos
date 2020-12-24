468e25838774be9c4962a9965852610a93c770a9
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:31:28 +0100

    Add files via upload
    24/12/2020 UD4_Ex12

---
+++ UD4_Ex12/UD4_Ex12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UD4_Ex12
+{
+    /* 12) Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida
+    por consola. Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Victor";
+            Console.WriteLine("Bienvenido {0}", nombre);
+        }
+    }
+}
