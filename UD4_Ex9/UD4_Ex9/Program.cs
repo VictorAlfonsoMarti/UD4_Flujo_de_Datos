de671cd50de981b8202bbf9841bff432cd43e94f
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:27:52 +0100

    Add files via upload
    24/12/2020 UD4_Ex9

---
+++ UD4_Ex9/UD4_Ex9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UD4_Ex9
+{
+    // 9) Recorre los números del 1 al 100. Muestra los números pares. Usar el tipo de bucle que quieras.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x=0; x<100; x++)
+            {
+                if ((x%2)==0) // Revisamos si es par
+                {
+                    Console.WriteLine(x);
+                }
+
+            }
+        }
+    }
+}
