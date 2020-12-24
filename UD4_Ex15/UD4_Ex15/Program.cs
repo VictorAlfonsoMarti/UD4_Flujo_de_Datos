c6bb332e2108b4ed1347714a495628c00296e160
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:35:54 +0100

    Add files via upload
    24/12/2020 UD4_Ex15

---
+++ UD4_Ex15/UD4_Ex15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UD4_Ex15
+{
+    /* 15) Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también
+    debemos indicarlo */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Número divisible entre 2? Introduce el número:  ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("Sí");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        
+        }
+    }
+}
