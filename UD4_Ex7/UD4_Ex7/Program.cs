fae29ad1ceebf9bd757a88b2d951490c5ba2a754
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:25:52 +0100

    Add files via upload
    24/12/2020 UD4_Ex7

---
+++ UD4_Ex7/UD4_Ex7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UD4_Ex7
+{
+    class Program
+    {
+        // 7) Recorre los números del 1 al 100. Usa un bucle for
+        static void Main(string[] args)
+        {
+            for (int x=1; x<=100; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
