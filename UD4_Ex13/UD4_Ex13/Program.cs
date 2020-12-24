dd68cbb123fe28788f7ce79c33a34721d976fb93
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:34:12 +0100

    Add files via upload
    24/12/2020 UD4_Ex13

---
+++ UD4_Ex13/UD4_Ex13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UD4_Ex13
+{
+    // 13) Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido {0}", nombre);
+        }
+    }
+}
