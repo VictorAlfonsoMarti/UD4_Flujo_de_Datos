22afc8b8c9a4edd3bb86287847d20250f5bb4358
Víctor Alfonso Martí <victor19am@gmail.com>
2020-12-24 14:19:27 +0100

    Add files via upload
    24/12/2020 UD4_Ex3

---
+++ UD4_Ex3/UD4_Ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UD4_Ex3
+{
+    class Program
+    {
+        /* 3) Pedir por consola tu nombre y tu edad y mostrar el siguiente mensaje: “Te
+        llamas ” <nombre> ” y tienes ” <años> ” años”. */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Nombre?");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("¿Edad?");
+            int edad = Convert.ToInt32(Console.ReadLine()); // Convertimos en int el string que recogemos. 
+
+            Console.WriteLine("Te llamas {0} y tienes {1} años.", nombre, edad);
+
+        }
+    }
+}
